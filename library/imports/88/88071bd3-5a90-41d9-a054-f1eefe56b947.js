40ab19f3f3a0cb9b82fb89da2b125dd35e18a71e
LCZ <373113210@qq.com>
2018-04-26 11:01:28 +0800

    update

---
+++ library/imports/88/88071bd3-5a90-41d9-a054-f1eefe56b947.js
@@ -0,0 +1,117 @@
+"use strict";
+cc._RF.push(module, '88071vTWpBB2aBU8e7+VrlH', 'Player');
+// scripts/Player.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        jumpHeight: 150,
+        jumpDuration: 0.3,
+        maxMoveSpeed: 400,
+        accel: 1000,
+        jumpAudio: {
+            default: null,
+            url: cc.AudioClip
+        }
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+    setJumpAction: function setJumpAction() {
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // 下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        var callback = cc.callFunc(this.playJumpSound, this);
+        // 不断重复，而且每次完成落地动作后调用回调来播放声音
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown, callback));
+    },
+    playJumpSound: function playJumpSound() {
+        // 调用声音引擎播放声音
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+    },
+    setInputControl: function setInputControl() {
+        var self = this;
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function (event) {
+            switch (event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = true;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = true;
+                    break;
+            }
+        });
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function (event) {
+            switch (event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = false;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = false;
+                    break;
+            }
+        });
+    },
+    update: function update(dt) {
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        this.node.x += this.xSpeed * dt;
+    },
+    onLoad: function onLoad() {
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        this.accLeft = false;
+        this.accRight = false;
+        this.xSpeed = 0;
+        this.setInputControl();
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {}
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
